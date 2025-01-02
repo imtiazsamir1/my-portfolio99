660d2ce65e73b9b234ec835e7f257bec9f68388c
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-01-02 16:39:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Protfolio1</section>
+    <section>Protfolio2</section>
+    <section>Protfolio3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
