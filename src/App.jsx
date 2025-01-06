24ec0b783473c782428fc9b2e5d541a4a9720bdb
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-01-06 22:27:50 +0600

    titleContainer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Hero from "./components/Hero/Hero";
 import Navber from "./components/Navber/Navber";
 import Test from "./components/Navber/Test";
 import Parallax from "./components/Parallax/Parallax";
+import Services from "./components/Services/Services";
 const App = () => {
  
   return <div>
    <section id="Homepage"><Navber></Navber>
    <Hero/></section>
-    <section id="Services"><Parallax type="services"/></section>
+    <section id="Services"><Parallax type="Services"/>
+    <Services></Services></section>
     <section id="Portfolio">Services</section>
     <section ><Parallax type="portfolio"/></section>
     <section>Protfolio1</section>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import "./services.scss";
-
+import {motion} from "framer-motion"
 const Services = () => {
     return (
-        <div>
+        <motion.div className='services'>
+            <motion.div className="textContainer">
+                <p>I Focus on helping your brand grow <br/>and move froward</p>
+                <hr/>
+            </motion.div>
+            <motion.div className="titleContainer">
+                <div className="title">
+                    <img src="/people.webp" alt="" />
+                    <h1><b>Unique</b></h1>
+                </div>
+                <div className="title">
+                   
+                    <h1><b>For Your</b>Business.</h1>
+                    <button>WHAT WE DO?</button>
+                </div>
+            </motion.div>
+            <motion.div className="listContainer">
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et obcaecati quis nam saepe architecto voluptatem itaque voluptatum quisquam ducimus in.</p>
+                    <button>GO</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et obcaecati quis nam saepe architecto voluptatem itaque voluptatum quisquam ducimus in.</p>
+                    <button>GO</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et obcaecati quis nam saepe architecto voluptatem itaque voluptatum quisquam ducimus in.</p>
+                    <button>GO</button>
+                </div>
+                <div className="box">
+                    <h2>Branding</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et obcaecati quis nam saepe architecto voluptatem itaque voluptatum quisquam ducimus in.</p>
+                    <button>GO</button>
+                </div>
+            </motion.div>
             Services
-        </div>
+        </motion.div>
     );
 };
 
