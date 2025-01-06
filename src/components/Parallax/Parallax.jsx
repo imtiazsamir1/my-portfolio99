7ea5303c076365c2e45997b63acd0b2019e3205d
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-01-06 21:42:54 +0600

    paralax

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -9,7 +9,8 @@ const {scrollYProgress}=useScroll({
     target:ref,
     offset:["start start","end start"]
 });
-const yBg= useTransform(scrollYProgress,[0,1],["0%","500%"])
+const yText= useTransform(scrollYProgress,[0,1],["0%","500%"])
+const yBg= useTransform(scrollYProgress,[0,1],["0%","100%"])
 
     return (
         <div className='parallax' 
@@ -19,10 +20,10 @@ const yBg= useTransform(scrollYProgress,[0,1],["0%","500%"])
             ?"linear-gradient(180deg,#111132,#0c0c1d"
             :"linear-gradient(180deg,#111132,#505064",
         }}>
-           <motion.h1 style={{y: yBg}} >{type==="services" ? "What we do?" :"What we did?"}</motion.h1>
+           <motion.h1 style={{y: yText}} >{type==="services" ? "What we do?" :"What we did?"}</motion.h1>
            <motion.div className='mountains'></motion.div>
-           <motion.div className='planets'></motion.div>
-           <motion.div className='stars'></motion.div>
+           <motion.div style={{y: yBg}} className='planets'></motion.div>
+           <motion.div style={{x: yBg}} className='stars'></motion.div>
         </div>
     );
 };
