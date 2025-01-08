fe1829b3679dfb9bf8751de879ebe09bc89679f5
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-01-08 22:28:19 +0600

    animation

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
 import "./services.scss";
 import {motion} from "framer-motion"
+
+
+ 
 const Services = () => {
+    const varients={ 
+        initial:{
+            x:-500,
+            y:100,
+            opacity:0
+        },
+        animate:{
+            x:0,
+            oppacity:1,
+            y:0,
+            transition:{
+                duration:1,
+                staggerChildren: 0.1,
+            }
+        }
+    
+    }
+    ;
+
     return (
-        <motion.div className='services'>
-            <motion.div className="textContainer">
+        <motion.div 
+        className='services' 
+        varients={varients} 
+        initial="initial" 
+        animate="animate">
+            <motion.div className="textContainer"varients={varients}>
                 <p>I Focus on helping your brand grow <br/>and move froward</p>
                 <hr/>
             </motion.div>
-            <motion.div className="titleContainer">
+            <motion.div className="titleContainer" varients={varients}>
                 <div className="title">
                     <img src="/people.webp" alt="" />
                     <h1><b>Unique</b></h1>
@@ -19,7 +45,7 @@ const Services = () => {
                     <button>WHAT WE DO?</button>
                 </div>
             </motion.div>
-            <motion.div className="listContainer">
+            <motion.div className="listContainer" varients={varients}>
                 <motion.div className="box" whileHover={{background:"lightgray",color:"black"}}>
                     <h2>Branding</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et obcaecati quis nam saepe architecto voluptatem itaque voluptatum quisquam ducimus in.</p>
