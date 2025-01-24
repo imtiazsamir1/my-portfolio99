9b7d5212ac59f9db11a2457b188466e33f502196
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-01-24 12:40:57 +0600

    featured

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero/Hero";
 import Navber from "./components/Navber/Navber";
 import Test from "./components/Navber/Test";
 import Parallax from "./components/Parallax/Parallax";
+import Portfoio from "./components/portfolio/Portfoio";
 import Services from "./components/Services/Services";
 const App = () => {
  
@@ -12,10 +13,9 @@ const App = () => {
     <section id="Services"><Parallax type="Services"/>
     <Services></Services></section>
     <section id="Portfolio">Services</section>
-    <section ><Parallax type="portfolio"/></section>
-    <section>Protfolio1</section>
-    <section>Protfolio2</section>
-    <section>Protfolio3</section>
+    <section ><Parallax type="portfolio"/>
+    </section>
+    <Portfoio/>
     <section id="Contact">Contact</section>
   
   </div>;
---
+++ src/components/portfolio/Portfoio.jsx
@@ -0,0 +1,70 @@
+import "./portfolio.scss"
+import React, { useRef } from 'react';
+import {motion,useScroll,useSpring} from "framer-motion";
+
+const items =[
+    {
+        id:1,
+        title:"React Commerce",
+        img:"https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        desc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+    },
+    {
+        id:2,
+        title:"React Commerce",
+        img:"https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        desc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+    },
+    {
+        id:3,
+        title:"React Commerce",
+        img:"https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        desc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+    },
+    {
+        id:4,
+        title:"React Commerce",
+        img:"https://images.pexels.com/photos/265087/pexels-photo-265087.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        desc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+
+    },
+];
+const Single =({item}) =>{
+    return(
+<section>
+    {item.title}
+</section>
+    )
+}
+
+
+const Portfoio = () => {
+    
+const ref = useRef()
+const {scrollYProgress}= useScroll({terget:ref, offset:["end end","start start"]});
+
+const scaleX =useSpring(scrollYProgress,
+    {
+        stiffness: 100,
+        damping: 30,
+    }
+)
+
+
+    return (
+        <div className="portfolio" ref={ref}>
+            <div className="progress">
+                <h1>Featured Works</h1>
+                <motion.div style={{scaleX}} className="progressBar"></motion.div>
+            </div>
+            {items.map((item)=>(
+                <Single item={item} key={item.id}/>
+            ))}
+        </div>
+    );
+};
+
+export default Portfoio;
